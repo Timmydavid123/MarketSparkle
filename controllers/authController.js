f24c4bb8be5e4f75f8aff71ba959234a13c76d2a
Timmydavid123 <114180127+Timmydavid123@users.noreply.github.com>
2023-12-15 16:59:14 +0100

    jhdn

--- controllers/authController.js
+++ controllers/authController.js
@@ -426,7 +426,7 @@
     await vendor.save();
 
     // Send the password reset email with the token link
-    const resetLink = `http://localhost:3000/Resetpassword?token=${passwordResetToken}`;
+    const resetLink = `http://localhost:3001/Resetpassword?token=${passwordResetToken}`;
     const emailText = `Click on the following link to reset your password: ${resetLink}`;
     await sendEmail(email, 'Password Reset', emailText);
 
