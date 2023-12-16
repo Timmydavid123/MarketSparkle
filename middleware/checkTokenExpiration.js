8c029c0aafe312553f19f8aaa5ae34f865afc519
Timmydavid123 <114180127+Timmydavid123@users.noreply.github.com>
2023-12-16 23:28:31 +0100

    nbhgfhb

--- middleware/checkTokenExpiration.js
+++ middleware/checkTokenExpiration.js
@@ -1,33 +1,48 @@
-// checkTokenExpiration.js
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET;
 
 module.exports = (req, res, next) => {
-  // Get the token from the request headers
-  const token = req.headers.authorization;
+  try {
+    // Get the token from the request headers
+    const token = req.headers.authorization;
 
-  if (token) {
+    if (!token) {
+      // No token provided, move to the next middleware or route
+      return next();
+    }
+
     // Verify the token
     jwt.verify(token, JWT_SECRET, (err, decoded) => {
       if (err) {
-        // Token verification failed, indicating an expired token
+        // Token verification failed
         console.error('Token verification failed:', err);
-        req.logout(); // Perform logout
-      } else {
 
-      
-        const currentTime = Date.now(); // Current time in milliseconds
-        const expirationTime = decoded.exp * 1000; // Convert seconds to milliseconds
-        
-        const oneDayInMilliseconds = 24 * 60 * 60 * 1000; // One day in milliseconds
-        
-        if (currentTime > expirationTime + oneDayInMilliseconds) {
-          console.error('Token expired after one day.');
-          req.logout(); // Perform logout
+        // Optionally, you can distinguish between token expiration and other verification failures
+        if (err.name === 'TokenExpiredError') {
+          // Token has expired
+          console.error('Token has expired.');
         }
-      }
-    });
-  }
 
-  next();
+        // Handle the error or log additional information
+        return res.status(401).json({ message: 'Invalid or expired token' });
+      }
+
+      // Token is valid, proceed with additional checks (if needed)
+      const currentTime = Date.now(); // Current time in milliseconds
+      const expirationTime = decoded.exp * 1000; // Convert seconds to milliseconds
+
+      const oneDayInMilliseconds = 24 * 60 * 60 * 1000; // One day in milliseconds
+
+      if (currentTime > expirationTime + oneDayInMilliseconds) {
+        console.error('Token expired after one day.');
+        return res.status(401).json({ message: 'Token expired after one day' });
+      }
+
+      // Token is valid and not expired, continue to the next middleware or route
+      next();
+    });
+  } catch (error) {
+    console.error('Error during token verification:', error);
+    res.status(500).json({ message: 'Internal Server Error during token verification' });
+  }
 };
